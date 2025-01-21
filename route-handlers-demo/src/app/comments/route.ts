07fbf72857bd02830c6bfaa99a2f3cd220654d4a
Safwene17 <safwenenacheb17@gmail.com>
2025-01-22 00:06:48 +0100

    fix

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,9 +1,12 @@
 import { text } from "stream/consumers";
 import { comments } from "./comments";
-import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request : NextRequest) {
+    const searchparams = request.nextUrl.searchParams;
+    const query = searchparams.get("query");
+    const filteredComments = query ? comments.filter((comment) => comment.text.includes(query)) : comments;
+    return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
