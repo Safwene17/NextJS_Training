f374030eb478eac6a058890328c2244549e16814
Safwene17 <safwenenacheb17@gmail.com>
2025-01-22 00:45:21 +0100

    cookies in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,8 +1,14 @@
-import { headers } from "next/headers";
+import { headers , cookies} from "next/headers";
 
 export async function GET(request: Request) {
+
     const HeadersList = await headers();
     console.log(HeadersList.get("Authorization"));
+
+    const cookiesList = await cookies();
+    cookiesList.set("ResultsPerPage", "20");
+    console.log(cookiesList.get("ResultsPerPage"));
+
     return new Response("<h1>Hello World</h1>",
          {
             headers: { "Content-Type": "text/html" },
